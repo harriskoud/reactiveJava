8634f3c397614e7d6fa82ce787564a6b8c58f17a
Charidimos Koudoumas <extern.charidimos.koudoumas@elektrobit.com>
2019-01-07 17:35:19 +0200

    reactive project

---
+++ src/test/java/com/koud/reactiveservice/ReactiveServiceApplicationTests.java
@@ -0,0 +1,17 @@
+package com.koud.reactiveservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ReactiveServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
